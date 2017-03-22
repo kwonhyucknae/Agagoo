2444a25f4d519288464a26812864a5cc4f966150
kwonhyucknae <nhk321@naver.com>
2017-03-23 02:04:36 +0900

    레이캐스트 해결 3d 오브젝트 + ui 클릭 가능

---
+++ Assets/Script/Raycast.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour {
+
+    public Camera ARCameraReference;
+    public GameObject Categorypage;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0))
+        {
+            Ray ray = ARCameraReference.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit))
+            {
+                Debug.Log("Hit : " + hit.collider.gameObject.name);
+                Categorypage.active = false;
+
+            }
+        }
+	}
+}
---
+++ Assets/Script/TouchEvent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchEvent : MonoBehaviour {
+    public float rotateRate;
+    public Texture2D Thumb;
+    private Vector2 PrevPoint;
+    private string TouchStatus;
+
+
+	// Use this for initialization
+	void Start () {
+        TouchStatus = "Idle";	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.touchCount==1)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                TouchStatus = "Touch Began";
+                PrevPoint = Input.GetTouch(0).position;
+            }
+            if(Input.GetTouch(0).phase==TouchPhase.Moved)
+            {
+                TouchStatus = "Touch Moved";
+                Debug.Log(PrevPoint + "->" + Input.GetTouch(0).position);
+
+                gameObject.transform.Rotate(Input.GetTouch(0).position.y - PrevPoint.y, -(Input.GetTouch(0).position.x - PrevPoint.x), 0);
+                PrevPoint = Input.GetTouch(0).position;
+            }
+        }
+        else
+        {
+            gameObject.transform.Translate(0, 0, 0);
+            TouchStatus = "Idle";
+        }
+	}
+}
