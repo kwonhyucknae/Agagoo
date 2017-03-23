a00ea92bb9d0204912abfb8392f6089b033adefe
kwonhyucknae <nhk321@naver.com>
2017-03-23 14:11:55 +0900

    sofa 터치로 회전까지 가능

--- Assets/Script/Raycast.cs
+++ Assets/Script/Raycast.cs
@@ -6,9 +6,21 @@ public class Raycast : MonoBehaviour {
 
     public Camera ARCameraReference;
     public GameObject Categorypage;
-	// Use this for initialization
-	void Start () {
-		
+
+    public float rotationSpeed = 10.0f;
+    public float lerpSpeed = 1.0f;
+
+    private Vector3 speed = new Vector3();
+    private Vector3 avgSpeed = new Vector3();
+    private bool dragging = false;
+
+    public GameObject sofa;
+
+    public float x;
+    public float y;
+
+    // Use this for initialization
+    void Start () {
 	}
 	
 	// Update is called once per frame
@@ -19,10 +31,38 @@ public class Raycast : MonoBehaviour {
             RaycastHit hit;
             if(Physics.Raycast(ray,out hit))
             {
+                
                 Debug.Log("Hit : " + hit.collider.gameObject.name);
-                Categorypage.active = false;
+                if (hit.collider.gameObject.name == "sofa")
+                {
+                    x = Input.GetAxis("Mouse X");
+                    y = Input.GetAxis("Mouse Y");
 
+                    if(Mathf.Abs(x)>=Mathf.Abs(y))
+                    {
+                        sofa.transform.Rotate(Vector3.up * -x);
+                    }
+                    else
+                    {
+                        sofa.transform.Rotate(Vector3.right * -y);
+                    }
+                    /*클릭으로 회전됨
+                   if(Input.GetMouseButtonDown(0))
+                    {
+                        Vector3 pos = Input.mousePosition;
+                        pos.z = 10;
+                        Vector3 target = Camera.main.ScreenToWorldPoint(pos);
+                        print("target" + target);
+                        sofa.transform.LookAt(target);
+                    }
+                    */
+                    //Categorypage.active = false;
+
+                    
+                }
             }
         }
 	}
+
+ 
 }
