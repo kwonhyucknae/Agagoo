f64d79bdb9ee8cd24c0cd68528dc79c7b67a9b61
kwonhyucknae <nhk321@naver.com>
2017-04-29 08:51:11 +0900

    커스텀 가구 어느정도 완료

--- Assets/Script/CustomerGagoo/ChangeTest.cs
+++ Assets/Script/CustomerGagoo/ChangeTest.cs
@@ -6,19 +6,88 @@ public class ChangeTest : MonoBehaviour {
     public GameObject lamp;
     public GameObject[] lampco1=new GameObject[8];
 
+    public int secondnum = 0;
+    public bool OnOff = false;
+
     public void change1(int num)
     {
         for (int i = 1; i <= 8; i++)
         {
-            GameObject.Find("SingleLamp_Type12").transform.FindChild("Base").transform.FindChild("Select Metal Atlas").transform.FindChild("SingleLampBase_Type12_Mat"+i).gameObject.SetActive(false);
+            GameObject.Find("SingleLamp_Type12(Clone)").transform.FindChild("Base").transform.FindChild("Select Metal Atlas").transform.FindChild("SingleLampBase_Type12_Mat"+i).gameObject.SetActive(false);
         }
-        GameObject.Find("SingleLamp_Type12").transform.FindChild("Base").transform.FindChild("Select Metal Atlas").transform.FindChild("SingleLampBase_Type12_Mat" + num).gameObject.SetActive(true);
+        GameObject.Find("SingleLamp_Type12(Clone)").transform.FindChild("Base").transform.FindChild("Select Metal Atlas").transform.FindChild("SingleLampBase_Type12_Mat" + num).gameObject.SetActive(true);
+    }
+    public void changeshape(int num)
+    {
+        for(int i=1;i<=11;i++)
+        {
+            GameObject.Find("SingleLamp_Type12(Clone)").transform.FindChild("LampScreens").transform.FindChild("Screen Select MODELS2").transform.FindChild("LampScreen_Type" + i).gameObject.SetActive(false);
+        }
+        GameObject.Find("SingleLamp_Type12(Clone)").transform.FindChild("LampScreens").transform.FindChild("Screen Select MODELS2").transform.FindChild("LampScreen_Type" +num).gameObject.SetActive(false);
+        secondnum = num;
+    }
+    public void changeOnOff(int num)
+    {
+        GameObject.Find("SingleLamp_Type12(Clone)").transform.FindChild("LampScreens").transform.FindChild("Screen Select MODELS2").transform.FindChild("LampScreen_Type" + secondnum).transform.FindChild("Turn Light SWITCH2").transform.FindChild("ON").gameObject.SetActive(false);
+        GameObject.Find("SingleLamp_Type12(Clone)").transform.FindChild("LampScreens").transform.FindChild("Screen Select MODELS2").transform.FindChild("LampScreen_Type" + secondnum).transform.FindChild("Turn Light SWITCH2").transform.FindChild("OFF").gameObject.SetActive(false);
+        if(num==1)
+        {
+            GameObject.Find("SingleLamp_Type12(Clone)").transform.FindChild("LampScreens").transform.FindChild("Screen Select MODELS2").transform.FindChild("LampScreen_Type" + secondnum).transform.FindChild("Turn Light SWITCH2").transform.FindChild("ON").gameObject.SetActive(true);
+            OnOff = true;
+        }
+        else
+        {
+            GameObject.Find("SingleLamp_Type12(Clone)").transform.FindChild("LampScreens").transform.FindChild("Screen Select MODELS2").transform.FindChild("LampScreen_Type" + secondnum).transform.FindChild("Turn Light SWITCH2").transform.FindChild("OFF").gameObject.SetActive(true);
+            OnOff = false;
+        }
+
+    }
+    public void changeColor(int num)
+    {
+        if(OnOff==true)
+        {
+            for(int i=1;i<=8;i++)
+            {
+                GameObject.Find("SingleLamp_Type12(Clone)").transform.FindChild("LampScreens").transform.FindChild("Screen Select MODELS2").transform.FindChild("LampScreen_Type" + secondnum).transform.FindChild("Turn Light SWITCH2").transform.FindChild("ON").transform.FindChild("Select Lamp Screen Atlas").transform.FindChild("LampScreen_Type2_Mat"+i).gameObject.SetActive(false);
+
+            }
+            GameObject.Find("SingleLamp_Type12(Clone)").transform.FindChild("LampScreens").transform.FindChild("Screen Select MODELS2").transform.FindChild("LampScreen_Type" + secondnum).transform.FindChild("Turn Light SWITCH2").transform.FindChild("ON").transform.FindChild("Select Lamp Screen Atlas").transform.FindChild("LampScreen_Type2_Mat" + num).gameObject.SetActive(true);
+
+        }
+        else if(OnOff==false)
+        {
+            for(int i=9;i<=16;i++)
+            {
+                GameObject.Find("SingleLamp_Type12(Clone)").transform.FindChild("LampScreens").transform.FindChild("Screen Select MODELS2").transform.FindChild("LampScreen_Type" + secondnum).transform.FindChild("Turn Light SWITCH2").transform.FindChild("ON").transform.FindChild("Select Lamp Screen Atlas").transform.FindChild("LampScreen_Type2_Mat" + i).gameObject.SetActive(false);
+
+            }
+            GameObject.Find("SingleLamp_Type12(Clone)").transform.FindChild("LampScreens").transform.FindChild("Screen Select MODELS2").transform.FindChild("LampScreen_Type" + secondnum).transform.FindChild("Turn Light SWITCH2").transform.FindChild("ON").transform.FindChild("Select Lamp Screen Atlas").transform.FindChild("LampScreen_Type2_Mat" + (num+8)).gameObject.SetActive(true);
+
+        }
+
+    }
+    public void changeBulbColor(int num)
+    {
+
+        GameObject.Find("SingleLamp_Type12(Clone)").transform.FindChild("LampScreens").transform.FindChild("Screen Select MODELS2").transform.FindChild("LampScreen_Type" + secondnum).transform.FindChild("Turn Light SWITCH2").transform.FindChild("ON").transform.FindChild("Bulb Color Selection SWITCH").transform.FindChild("Bulb_white").gameObject.SetActive(false);
+        GameObject.Find("SingleLamp_Type12(Clone)").transform.FindChild("LampScreens").transform.FindChild("Screen Select MODELS2").transform.FindChild("LampScreen_Type" + secondnum).transform.FindChild("Turn Light SWITCH2").transform.FindChild("ON").transform.FindChild("Bulb Color Selection SWITCH").transform.FindChild("Bulb_yellow").gameObject.SetActive(false);
+
+        if(num==1)
+        {
+            GameObject.Find("SingleLamp_Type12(Clone)").transform.FindChild("LampScreens").transform.FindChild("Screen Select MODELS2").transform.FindChild("LampScreen_Type" + secondnum).transform.FindChild("Turn Light SWITCH2").transform.FindChild("ON").transform.FindChild("Bulb Color Selection SWITCH").transform.FindChild("Bulb_white").gameObject.SetActive(true);
+
+        }
+        else
+        {
+            GameObject.Find("SingleLamp_Type12(Clone)").transform.FindChild("LampScreens").transform.FindChild("Screen Select MODELS2").transform.FindChild("LampScreen_Type" + secondnum).transform.FindChild("Turn Light SWITCH2").transform.FindChild("ON").transform.FindChild("Bulb Color Selection SWITCH").transform.FindChild("Bulb_yellow").gameObject.SetActive(true);
+
+        }
     }
 
     // Use this for initialization
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
